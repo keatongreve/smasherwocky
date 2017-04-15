a814074804bffbfdb758fa883952190afb5a7ed0
Keaton Greve <keatongreve@gmail.com>
2017-04-15 15:49:01 -0500

    formatting

--- api/controllers/MatchController.js
+++ api/controllers/MatchController.js
@@ -7,105 +7,128 @@ var MatchModel = require('../models/MatchModel.js');
  */
 module.exports = {
 
-    /**
-     * MatchController.list()
-     */
-    list: function (req, res) {
-        MatchModel.find(function (err, Matchs) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Match.',
-                    error: err
-                });
-            }
-            return res.json(Matchs);
+  /**
+   * MatchController.list()
+   */
+  list: function(req, res) {
+    MatchModel.find(function(err, Matchs) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting Match.',
+          error: err
         });
-    },
+      }
+      return res.json(Matchs);
+    });
+  },
 
-    /**
-     * MatchController.show()
-     */
-    show: function (req, res) {
-        var id = req.params.id;
-        MatchModel.findOne({_id: id}, function (err, Match) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Match.',
-                    error: err
-                });
-            }
-            if (!Match) {
-                return res.status(404).json({
-                    message: 'No such Match'
-                });
-            }
-            return res.json(Match);
+  /**
+   * MatchController.show()
+   */
+  show: function(req, res) {
+    var id = req.params.id;
+    MatchModel.findOne({
+      _id: id
+    }, function(err, Match) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting Match.',
+          error: err
         });
-    },
-
-    /**
-     * MatchController.create()
-     */
-    create: function (req, res) {
-        var Match = new MatchModel({			player1 : req.body.player1,			player2 : req.body.player2,			player1Character : req.body.player1Character,			player2Character : req.body.player2Character,			player1EndStocks : req.body.player1EndStocks,			player2EndStocks : req.body.player2EndStocks,			winner : req.body.winner,			date : req.body.date
+      }
+      if (!Match) {
+        return res.status(404).json({
+          message: 'No such Match'
         });
+      }
+      return res.json(Match);
+    });
+  },
 
-        Match.save(function (err, Match) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when creating Match',
-                    error: err
-                });
-            }
-            return res.status(201).json(Match);
+  /**
+   * MatchController.create()
+   */
+  create: function(req, res) {
+    var Match = new MatchModel({
+      player1: req.body.player1,
+      player2: req.body.player2,
+      player1Character: req.body.player1Character,
+      player2Character: req.body.player2Character,
+      player1EndStocks: req.body.player1EndStocks,
+      player2EndStocks: req.body.player2EndStocks,
+      winner: req.body.winner,
+      date: req.body.date
+    });
+
+    Match.save(function(err, Match) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when creating Match',
+          error: err
         });
-    },
+      }
+      return res.status(201).json(Match);
+    });
+  },
 
-    /**
-     * MatchController.update()
-     */
-    update: function (req, res) {
-        var id = req.params.id;
-        MatchModel.findOne({_id: id}, function (err, Match) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Match',
-                    error: err
-                });
-            }
-            if (!Match) {
-                return res.status(404).json({
-                    message: 'No such Match'
-                });
-            }
-
-            Match.player1 = req.body.player1 ? req.body.player1 : Match.player1;			Match.player2 = req.body.player2 ? req.body.player2 : Match.player2;			Match.player1Character = req.body.player1Character ? req.body.player1Character : Match.player1Character;			Match.player2Character = req.body.player2Character ? req.body.player2Character : Match.player2Character;			Match.player1EndStocks = req.body.player1EndStocks ? req.body.player1EndStocks : Match.player1EndStocks;			Match.player2EndStocks = req.body.player2EndStocks ? req.body.player2EndStocks : Match.player2EndStocks;			Match.winner = req.body.winner ? req.body.winner : Match.winner;			Match.date = req.body.date ? req.body.date : Match.date;			
-            Match.save(function (err, Match) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error when updating Match.',
-                        error: err
-                    });
-                }
-
-                return res.json(Match);
-            });
+  /**
+   * MatchController.update()
+   */
+  update: function(req, res) {
+    var id = req.params.id;
+    MatchModel.findOne({
+      _id: id
+    }, function(err, Match) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting Match',
+          error: err
         });
-    },
-
-    /**
-     * MatchController.remove()
-     */
-    remove: function (req, res) {
-        var id = req.params.id;
-        MatchModel.findByIdAndRemove(id, function (err, Match) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the Match.',
-                    error: err
-                });
-            }
-            return res.status(204).json();
+      }
+      if (!Match) {
+        return res.status(404).json({
+          message: 'No such Match'
         });
-    }
+      }
+
+      Match.player1 = req.body.player1 ? req.body.player1 : Match.player1;
+      Match.player2 = req.body.player2 ? req.body.player2 : Match.player2;
+      Match.player1Character = req.body.player1Character ? req.body.player1Character :
+        Match.player1Character;
+      Match.player2Character = req.body.player2Character ? req.body.player2Character :
+        Match.player2Character;
+      Match.player1EndStocks = req.body.player1EndStocks ? req.body.player1EndStocks :
+        Match.player1EndStocks;
+      Match.player2EndStocks = req.body.player2EndStocks ? req.body.player2EndStocks :
+        Match.player2EndStocks;
+      Match.winner = req.body.winner ? req.body.winner : Match.winner;
+      Match.date = req.body.date ? req.body.date : Match.date;
+      Match.save(function(err, Match) {
+        if (err) {
+          return res.status(500).json({
+            message: 'Error when updating Match.',
+            error: err
+          });
+        }
+
+        return res.json(Match);
+      });
+    });
+  },
+
+  /**
+   * MatchController.remove()
+   */
+  remove: function(req, res) {
+    var id = req.params.id;
+    MatchModel.findByIdAndRemove(id, function(err, Match) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when deleting the Match.',
+          error: err
+        });
+      }
+      return res.status(204).json();
+    });
+  }
 };
--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,105 +7,111 @@ var UserModel = require('../models/UserModel.js');
  */
 module.exports = {
 
-    /**
-     * UserController.list()
-     */
-    list: function (req, res) {
-        UserModel.find(function (err, Users) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting User.',
-                    error: err
-                });
-            }
-            return res.json(Users);
+  /**
+   * UserController.list()
+   */
+  list: function(req, res) {
+    UserModel.find(function(err, Users) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting User.',
+          error: err
         });
-    },
+      }
+      return res.json(Users);
+    });
+  },
 
-    /**
-     * UserController.show()
-     */
-    show: function (req, res) {
-        var id = req.params.id;
-        UserModel.findOne({_id: id}, function (err, User) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting User.',
-                    error: err
-                });
-            }
-            if (!User) {
-                return res.status(404).json({
-                    message: 'No such User'
-                });
-            }
-            return res.json(User);
+  /**
+   * UserController.show()
+   */
+  show: function(req, res) {
+    var id = req.params.id;
+    UserModel.findOne({
+      _id: id
+    }, function(err, User) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting User.',
+          error: err
         });
-    },
-
-    /**
-     * UserController.create()
-     */
-    create: function (req, res) {
-        var User = new UserModel({			name : req.body.name
+      }
+      if (!User) {
+        return res.status(404).json({
+          message: 'No such User'
         });
+      }
+      return res.json(User);
+    });
+  },
 
-        User.save(function (err, User) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when creating User',
-                    error: err
-                });
-            }
-            return res.status(201).json(User);
+  /**
+   * UserController.create()
+   */
+  create: function(req, res) {
+    var User = new UserModel({
+      name: req.body.name
+    });
+
+    User.save(function(err, User) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when creating User',
+          error: err
         });
-    },
+      }
+      return res.status(201).json(User);
+    });
+  },
 
-    /**
-     * UserController.update()
-     */
-    update: function (req, res) {
-        var id = req.params.id;
-        UserModel.findOne({_id: id}, function (err, User) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting User',
-                    error: err
-                });
-            }
-            if (!User) {
-                return res.status(404).json({
-                    message: 'No such User'
-                });
-            }
-
-            User.name = req.body.name ? req.body.name : User.name;			
-            User.save(function (err, User) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error when updating User.',
-                        error: err
-                    });
-                }
-
-                return res.json(User);
-            });
+  /**
+   * UserController.update()
+   */
+  update: function(req, res) {
+    var id = req.params.id;
+    UserModel.findOne({
+      _id: id
+    }, function(err, User) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when getting User',
+          error: err
         });
-    },
+      }
 
-    /**
-     * UserController.remove()
-     */
-    remove: function (req, res) {
-        var id = req.params.id;
-        UserModel.findByIdAndRemove(id, function (err, User) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the User.',
-                    error: err
-                });
-            }
-            return res.status(204).json();
+      if (!User) {
+        return res.status(404).json({
+          message: 'No such User'
         });
-    }
+      }
+
+      User.name = req.body.name ? req.body.name : User.name;
+      User.save(function(err, User) {
+        if (err) {
+          return res.status(500).json({
+            message: 'Error when updating User.',
+            error: err
+          });
+        }
+
+        return res.json(User);
+      });
+    });
+  },
+
+  /**
+   * UserController.remove()
+   */
+  remove: function(req, res) {
+    var id = req.params.id;
+    UserModel.findByIdAndRemove(id, function(err, User) {
+      if (err) {
+        return res.status(500).json({
+          message: 'Error when deleting the User.',
+          error: err
+        });
+      }
+      return res.status(204).json();
+    });
+  }
 };
