9d2eb37b3ce2a301ac9e405643a4e4b0b4407483
Keaton Greve <keatongreve@gmail.com>
2017-04-15 16:59:03 -0500

    Add server-side React view rendering engine

--- app.js
+++ app.js
@@ -20,4 +20,10 @@ app.use(cookieParser())
 app.use('/api/users', userRoutes);
 app.use('/api/matches', matchRoutes);
 
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.get('/', require('./app/routes').index);
+
 module.exports = app;
